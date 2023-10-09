8247b3d0a5dda352664da45927b29f38316d625d
“Melvin <melvintorresg@gmail.com>
2023-10-08 18:13:31 -0600

    :seedling: Agregando Archivos

---
+++ config/Config.php
@@ -0,0 +1,14 @@
+<?php
+define('BASE_URL', 'http://192.168.0.25/melvin/');
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DBNAME', 'melvin');
+define('CHARSET', 'charset=utf8');
+define('TITLE', 'COTIZADOR');
+define('MONEDA', 'Q. ');
+define('HOST_SMTP', 'smtp.gmail.com');
+define('USER_SMTP', 'tucorreo@gmail.com');
+define('CLAVE_SMTP', 'clave_generada');
+define('PUERTO_SMTP', 465);
+?>
